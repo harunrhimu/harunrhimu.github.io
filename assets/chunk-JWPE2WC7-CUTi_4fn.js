7bc6fd0356091c8ad19a94a18bb228025c441dbb
Md Harun Or Roshid <harunrhimu@gmail.com>
2026-08-04 19:40:32 +0600

    Add unique shareable URLs for each dashboard in gallery

---
+++ assets/chunk-JWPE2WC7-CUTi_4fn.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./CaseStudyDetail-C_j60ojO.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
--- assets/swimlanesDiagram-G3AALYLV-ChQHyVmQ.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-DOEFdh3-.js";import{_ as o}from"./CaseStudyDetail-D4cL0LrG.js";import"./chunk-5VM5RSS4-CA7NWofe.js";import"./chunk-XXDRQBXY-B3X8_b0c.js";import"./chunk-VR4S4FIN-BhukeCsP.js";import"./chunk-32BRIVSS-Doi35_7c.js";import"./channel-CXJWLkF6.js";import"./index-Do6U00sm.js";import"./caseStudies-B7GQOZby.js";import"./SEO-p1sL8RIJ.js";import"./Breadcrumb-C3U-eD9_.js";import"./structuredData-BtZCPKHO.js";var a=o(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=a,v=r({defaultLayout:"swimlane",styles:m});export{v as diagram};
