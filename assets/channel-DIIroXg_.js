6f538fff4a222922d1688164b27f4930cffe3ff9
Md Harun Or Roshid <harunrhimu@gmail.com>
2026-09-08 08:20:18 +0600

    Add light/dark theme with warm off-white palette

---
+++ assets/channel-DIIroXg_.js
@@ -0,0 +1,1 @@
+import{ag as o,ah as n}from"./CaseStudyDetail-DuEKFL5G.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
---
+++ assets/swimlanesDiagram-G3AALYLV-6x49-e8E.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-BgZj66oU.js";import{_ as o}from"./CaseStudyDetail-DuEKFL5G.js";import"./chunk-5VM5RSS4-CmwJj6Jc.js";import"./chunk-XXDRQBXY-kI9dcPbC.js";import"./chunk-VR4S4FIN-DwdPU7s1.js";import"./chunk-32BRIVSS-Dwm3j6Pl.js";import"./channel-DIIroXg_.js";import"./index-DQHcRVcO.js";import"./caseStudies-B7GQOZby.js";import"./SEO-BckwCF7C.js";import"./Breadcrumb-CGMD22sk.js";import"./structuredData-CFPKf96d.js";var a=o(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,v=r({defaultLayout:"swimlane",styles:m});export{v as diagram};
